2d53fe0db57c1d0630cd32dc3e836e2bbb3c6a45
mikael <mikaelhirpo@gmail.com>
2022-07-24 23:58:20 +0300

    app.js changes

--- App.js
+++ App.js
@@ -1,112 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { Component } from "react";
 import {
-  SafeAreaView,
-  ScrollView,
+  View,
+  Text,
   StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  PermissionsAndroid,
+} from "react-native";
+import Tuner from "./src/tuner";
+import Note from "./src/note";
+import Meter from "./src/meter";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+export default class App extends Component {
+  state = {
+    note: {
+      name: "A",
+      octave: 4,
+      frequency: 440,
+    },
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  _update(note) {
+    this.setState({ note });
+  }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  async componentDidMount() {
+    if (Platform.OS === "android") {
+      await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      ]);
+    }
+
+    const tuner = new Tuner();
+    tuner.start();
+    tuner.onNoteDetected = (note) => {
+      if (this._lastNoteName === note.name) {
+        this._update(note);
+      } else {
+        this._lastNoteName = note.name;
+      }
+    };
+  }
+
+  render() {
+    return (
+      <View style={style.body}>
+        <StatusBar backgroundColor="#000" translucent />
+        <Meter cents={this.state.note.cents} />
+        <Note {...this.state.note} />
+        <Text style={style.frequency}>
+          {this.state.note.frequency.toFixed(1)} Hz
+        </Text>
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  frequency: {
+    fontSize: 28,
+    color: "#37474f",
   },
 });
-
-export default App;
